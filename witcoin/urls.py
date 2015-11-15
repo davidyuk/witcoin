ae693ebdc0c5bf6f9fe1c8805625ee5106136ea4
Denis <denis_davidyuk@hotmail.com>
2015-11-16 01:07:16 +1000

    Add index page

--- witcoin/urls.py
+++ witcoin/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'', include('main.urls', namespace="main")),
     url(r'^admin/', include(admin.site.urls)),
 ]
