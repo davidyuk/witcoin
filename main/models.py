4cb2397bb4157fbfa6e41b20aecf55fe40ac529b
Denis <denis_davidyuk@hotmail.com>
2015-12-14 00:27:25 +1000

    Partly undo last commit, because bank user display incorrectly

--- main/models.py
+++ main/models.py
@@ -32,7 +32,9 @@ class UserProfile(models.Model):
         return self.earned() - self.spend()
 
     def __str__(self):
-        return self.user.first_name + ' ' + self.user.last_name
+        s = self.user.first_name + ' ' + self.user.last_name
+        s = self.user.username if s == ' ' else s
+        return s
 
     class Meta:
         ordering = ['user__first_name']
