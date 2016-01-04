1f65fad498cb1c8b6609b17957335bf956c0c68c
Denis <denis_davidyuk@hotmail.com>
2016-01-04 17:54:43 +1000

    Add filter to transactions page

--- main/urls.py
+++ main/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     url(r'^fefu$', views.fefu_send_mail, name='fefu_send_mail'),
     url(r'^fefu/(?P<token>[0-9A-z]{32})$', views.fefu_from_mail, name='fefu_from_mail'),
 
-    url(r'^transaction/all$', views.TransactionListView.as_view(), name='transaction_all'),
+    url(r'^transaction/all$', views.TransactionFilterView.as_view(), name='transaction_all'),
     url(r'^transaction/create$', views.transaction_create, name='transaction_create'),
     url(r'^transaction/(?P<pk>[0-9]+)$', views.transaction, name='transaction'),
 
--- main/views/transaction.py
+++ main/views/transaction.py
@@ -5,7 +5,12 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from ..forms import TransactionCreationForm
 from django.utils import timezone
-from django.views.generic.list import ListView
+from django_filters import FilterSet, MethodFilter, TypedChoiceFilter, RangeFilter, DateFromToRangeFilter
+from django_filters.views import FilterView
+from django.db.models import Q
+from distutils.util import strtobool
+from functools import reduce
+from operator import and_, or_
 
 
 @login_required
@@ -45,8 +50,57 @@ def transaction(request, pk):
     })
 
 
-class TransactionListView(ListView):
-    model = Transaction
+class TransactionFilterView(FilterView):
     paginate_by = 20
     template_name = 'main/transaction/all.html'
-    ordering = '-timestamp_create'
+
+    class filterset_class(FilterSet):
+        amount = RangeFilter(label='Сумма')
+        user = MethodFilter(label='Участник')
+        user_to = MethodFilter(label='Получатель')
+        user_from = MethodFilter(label='Отправитель')
+        timestamp_create = DateFromToRangeFilter(label='Дата создания')
+        STATUS_CHOICES = (
+            (None, 'Все'),
+            (True, 'Подтверждённые'),
+            (False, 'Отменённые'),
+        )
+        status = TypedChoiceFilter(label='Статус', choices=STATUS_CHOICES, coerce=strtobool)
+
+        @staticmethod
+        def get_user_to_q(value):
+            return Q(user_to__user__first_name__contains=value) | Q(user_to__user__last_name__contains=value)
+
+        @staticmethod
+        def get_user_from_q(value):
+            return Q(user_from__user__first_name__contains=value) | Q(user_from__user__last_name__contains=value)
+
+        @staticmethod
+        def get_user_q(value, q_list):
+            return reduce(and_, map(lambda s: reduce(or_, map(lambda f: f(s), q_list), Q()), value.split()), Q())
+
+        def filter_user(self, queryset, value):
+            return queryset.filter(self.get_user_q(value, [self.get_user_to_q, self.get_user_from_q]))
+
+        def filter_user_to(self, queryset, value):
+            return queryset.filter(self.get_user_q(value, [self.get_user_to_q]))
+
+        def filter_user_from(self, queryset, value):
+            return queryset.filter(self.get_user_q(value, [self.get_user_from_q]))
+
+        def __init__(self, *args, **kwargs):
+            # remove useless help_text 'Filter'
+            # https://github.com/alex/django-filter/issues/292
+            for _, f in self.base_filters.items():
+                f.field.help_text = {}
+            super().__init__(*args, **kwargs)
+            self.form.fields['o'].label = 'Сортировать по'
+
+        class Meta:
+            model = Transaction
+            fields = ['status', 'amount', 'timestamp_create']
+            order_by = (
+                ('-timestamp_create', 'Дата создания'),
+                ('-amount', 'Сумма операции'),
+                ('-status', 'Статус'),
+            )
