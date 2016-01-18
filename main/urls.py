174cc8e5eaa42676c2c18bd392eafeb215b1fe13
Denis <denis_davidyuk@hotmail.com>
2016-01-18 12:32:41 +1000

    Fix regex for user url
    In django.contrib.auth.models for validation of username field using
    '^[\w.@+-]+$' regex.

--- main/urls.py
+++ main/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     url(r'^home$', views.home, name='home'),
     url(r'^settings$', views.settings, name='settings'),
     url(r'^logout$', auth_views.logout, {'next_page': '/'}, name='logout'),
-    url(r'^user/(?P<username>[\w\d.-]+)$', views.user, name='user'),
+    url(r'^user/(?P<username>[\w.@+-]+)$', views.user, name='user'),
 
     url(r'^reset-password/done$', views.password_reset_done, name='password_reset_done'),
     url(r'^reset-password$', auth_views.password_reset, name='password_reset'),
