9b14fa91648248607ee52bb1a7b4c84d7d78cf97
Denis <denis_davidyuk@hotmail.com>
2016-08-21 13:07:32 +1000

    Not found page

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -4,10 +4,13 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import AppContainer from '../../ui/containers/AppContainer';
 import HomePageContainer from '../../ui/containers/HomePageContainer';
 
+import NotFoundPage from '../../ui/pages/NotFoundPage';
+
 export const renderRoutes = () => (
   <Router history={browserHistory}>
     <Route path="/" component={AppContainer}>
       <IndexRoute component={HomePageContainer} />
+      <Route path="*" component={NotFoundPage} />
     </Route>
   </Router>
 );
---
+++ imports/ui/pages/NotFoundPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NotFoundPage = () => (
+  <p>Страница не найдена.</p>
+);
+
+export default NotFoundPage;
