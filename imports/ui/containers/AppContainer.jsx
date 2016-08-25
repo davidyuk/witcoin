74e1a31bf7720e4e8e9d4b533505377aba7e6636
Denis <denis_davidyuk@hotmail.com>
2016-08-25 13:30:06 +1000

    Render application after user changes

--- imports/ui/containers/AppContainer.jsx
+++ imports/ui/containers/AppContainer.jsx
@@ -2,6 +2,8 @@ import { createContainer } from 'meteor/react-meteor-data';
 import App from '../layouts/App.jsx';
 
 export default createContainer(() => {
+  const user = Meteor.user();
   return {
+    user,
   };
 }, App);
