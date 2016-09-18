a2defb01f09304eb6e30544aa359fb027304714b
Denis <denis_davidyuk@hotmail.com>
2016-09-18 17:17:16 +1000

    chats: Add dev-only API `generate.messages` method

--- imports/api/testData.js
+++ imports/api/testData.js
@@ -3,6 +3,7 @@ import faker from 'faker';
 import { resetDatabase } from 'meteor/xolvio:cleaner';
 
 import './users';
+import { methods } from './chats';
 
 if (Meteor.isDevelopment) {
   faker.locale = 'ru';
@@ -10,6 +11,7 @@ if (Meteor.isDevelopment) {
   Meteor.methods({
     'reset.database': resetDatabase,
     'generate.users': generateUsers,
+    'generate.messages': generateMessages,
   });
 }
 
@@ -17,4 +19,17 @@ function generateUsers() {
   const f = Meteor.users.find().count() + 1;
   for (let i = f; i < f + 20; i++)
     Factory.create('user', {emails: [{address: `user${i}@example.com`}]});
+}
+
+function generateMessages() {
+  let users = Meteor.users.find().fetch();
+  users.forEach(function(user) {
+    for (let i = 0; i < 20; i++) {
+      const user2Id = users[faker.random.number(users.length - 1)]._id;
+      if (user._id == user2Id) return;
+      const chatId = methods['chat.get'].call({userId: user._id}, [user2Id]);
+      for (let i = 0; i < faker.random.number(100); i++)
+        Factory.create('message', { chatId });
+    }
+  });
 }
