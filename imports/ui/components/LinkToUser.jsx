2c43564a6b78bb9a7acb2c9398bf93928da87953
Denis <denis_davidyuk@hotmail.com>
2016-10-04 14:31:20 +1000

    Inflect user name using petrovich.js

--- imports/ui/components/LinkToUser.jsx
+++ imports/ui/components/LinkToUser.jsx
@@ -7,12 +7,15 @@ export default class LinkToUser extends React.Component {
   render() {
     return (
       <Link to={"/u/" + this.props.user._id}>
-        <UserName user={this.props.user} />
+        <UserName user={this.props.user} inflection={this.props.inflection} />
       </Link>
     );
   }
 }
 
+LinkToUser.inflections = UserName.inflections;
+
 LinkToUser.propTypes = {
   user: React.PropTypes.object.isRequired,
+  inflection: React.PropTypes.string,
 };
--- imports/ui/components/UserName.jsx
+++ imports/ui/components/UserName.jsx
@@ -1,17 +1,39 @@
 import React from 'react';
+import petrovich from 'petrovich';
 
 export default class UserName extends React.Component {
-  render() {
+  getName() {
     const user = this.props.user;
+    if (!user.profile) return user._id;
+    let u = {
+      first: user.profile.firstName,
+      last: user.profile.lastName,
+      gender: user.profile.gender || Meteor.users.genderTypes.MALE,
+    };
+    if (this.props.inflection)
+      u = petrovich(u, this.props.inflection);
+    return [u.first, u.last].join(' ');
+  }
 
+  render() {
     return (
       <span>
-        { user.profile ? user.profile.name : user._id }
+        { this.getName() }
       </span>
     );
   }
 }
 
+UserName.inflections = {
+  NOMINATIVE: 'nominative',
+  GENITIVE: 'genitive',
+  DATIVE: 'dative',
+  ACCUSATIVE: 'accusative',
+  INSTRUMENTAL: 'instrumental',
+  PREPOSITIONAL: 'prepositional',
+};
+
 UserName.propTypes = {
   user: React.PropTypes.object.isRequired,
+  inflection: React.PropTypes.string,
 };
