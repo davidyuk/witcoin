00cd2c7deb4d8d8aae1293cec847cdb3666c1190
Denis <denis_davidyuk@hotmail.com>
2016-10-15 23:53:13 +1000

    MessageInput: Refactor, add `buttonText` property

--- imports/ui/components/MessageInput.jsx
+++ imports/ui/components/MessageInput.jsx
@@ -1,34 +1,34 @@
 import React from 'react';
 
-export default class MessageInput extends React.Component {
-  onSubmit(event) {
+const MessageInput = ({handler, placeholder, buttonText}) => {
+  const onSubmit = event => {
     event.preventDefault();
     if (!event.target.content.value) return;
-    this.props.handler(event.target.content.value);
+    handler(event.target.content.value);
     event.target.content.value = '';
-  }
+  };
 
-  onKeyDown(event) {
+  const onKeyDown = event => {
     if (event.keyCode == 13 && !event.shiftKey) {
       event.preventDefault();
       event.target.form.submit.click();
     }
-  }
+  };
 
-  render() {
-    return (
-      <form onKeyDown={this.onKeyDown} onSubmit={this.onSubmit.bind(this)} style={{padding: '5px 0', flexShrink: 0}}>
-        <div className="form-group" style={{marginBottom: 5 + 'px'}}>
-          <textarea className="form-control input-sm" name="content" placeholder={this.props.placeholder}
-                    style={{minHeight: 50 + 'px'}}/>
-        </div>
-        <input type="submit" className="btn btn-primary btn-sm" name="submit" value="Отправить"/>
-      </form>
-    );
-  }
-}
+  const style = {minHeight: 50 + 'px', margin: '5px 0'};
+
+  return (
+    <form onKeyDown={onKeyDown} onSubmit={onSubmit} style={{flexShrink: 0}}>
+      <textarea className="form-control input-sm" name="content" {...{placeholder, style}} />
+      <input type="submit" className="btn btn-primary btn-sm" name="submit" value={buttonText || 'Отправить'} />
+    </form>
+  );
+};
 
 MessageInput.propTypes = {
-  handler: React.PropTypes.func,
+  handler: React.PropTypes.func.isRequired,
   placeholder: React.PropTypes.string,
+  buttonText: React.PropTypes.string,
 };
+
+export default MessageInput;
