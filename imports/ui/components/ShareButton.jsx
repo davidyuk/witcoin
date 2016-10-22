eb5ec175ca0f756a2c1fdebb397608db003aa622
Denis <denis_davidyuk@hotmail.com>
2016-10-22 21:55:47 +1000

    ShareButton: Add modal with input for comment

--- imports/ui/components/ShareButton.jsx
+++ imports/ui/components/ShareButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default class ShareButton extends React.Component {
   share() {
-    Meteor.call('action.share', this.props.action._id);
+    Meteor.call('action.share', this.props.action._id, this._input.value);
   }
 
   renderSharesCount() {
@@ -17,15 +17,37 @@ export default class ShareButton extends React.Component {
   render() {
     const action = this.props.action;
     const shared = action.currentUserShared;
+    const modalId = 'share_modal_' + action._id;
 
     return (
-      <div className="btn-group btn-group-xs">
-        {this.renderSharesCount()}
-        <button onClick={this.share.bind(this)} title="Поделиться"
-                className={'btn btn-default' + (shared ? ' active' : '')}>
-          <span className="glyphicon glyphicon-share-alt" />
-        </button>
-      </div>
+      <span>
+        <div className="modal fade" id={modalId} style={{textAlign: 'left'}}>
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <button type="button" className="close" data-dismiss="modal">&times;</button>
+                <h4 className="modal-title">Поделиться действием</h4>
+              </div>
+              <div className="modal-body">
+                <textarea ref={c => this._input = c} className="form-control" placeholder="Ваш комментарий" />
+              </div>
+              <div className="modal-footer">
+                <button onClick={this.share.bind(this)} className="btn btn-primary" data-dismiss="modal">
+                  Сохранить
+                </button>
+                <button className="btn btn-default" data-dismiss="modal">Отмена</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="btn-group btn-group-xs">
+          {this.renderSharesCount()}
+          <button title="Поделиться" data-toggle="modal" data-target={'#' + modalId}
+                  className={'btn btn-default' + (shared ? ' active' : '')}>
+            <span className="glyphicon glyphicon-share-alt" />
+          </button>
+        </div>
+      </span>
     );
   }
 }
