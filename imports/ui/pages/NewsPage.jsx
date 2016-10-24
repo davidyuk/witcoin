80365bd3f099dc30149deb84a08acb4c494ea9d2
Denis <denis_davidyuk@hotmail.com>
2016-10-24 23:57:29 +1000

    NewsPage: Refactor as pure function

--- imports/ui/pages/NewsPage.jsx
+++ imports/ui/pages/NewsPage.jsx
@@ -4,14 +4,9 @@ import InfiniteScroll from '../components/InfiniteScroll';
 import FeedListContainer from '../containers/FeedListContainer';
 import ActionTypeFilter from '../components/ActionTypeFilter';
 
-export default class NewsPage extends React.Component {
-  render() {
-    return (
-      <InfiniteScroll>
-        <ActionTypeFilter>
-          <FeedListContainer selector={{isNotification: false}} onEmptyMessage="Новости не найдены" />
-        </ActionTypeFilter>
-      </InfiniteScroll>
-    );
-  }
-}
+export default () =>
+  <InfiniteScroll>
+    <ActionTypeFilter>
+      <FeedListContainer selector={{isNotification: false}} onEmptyMessage="Новости не найдены" />
+    </ActionTypeFilter>
+  </InfiniteScroll>;
