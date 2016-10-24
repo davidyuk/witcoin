1d962ca11b4b548ecc0cc657ce2589909f2f889d
Denis <denis_davidyuk@hotmail.com>
2016-10-24 18:16:56 +1000

    App layout: Refactor as pure function

--- imports/ui/layouts/App.jsx
+++ imports/ui/layouts/App.jsx
@@ -1,50 +1,47 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router';
 
 import UserMenu from '../components/UserMenu';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <nav className="navbar navbar-default">
-          <div className="container">
-            <div className="navbar-header">
-              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
-                <span className="icon-bar"/>
-                <span className="icon-bar"/>
-                <span className="icon-bar"/>
-              </button>
-              <Link to="/" className="navbar-brand">Кленинка</Link>
-            </div>
-            <div id="navbar" className="collapse navbar-collapse">
-              <ul className="nav navbar-nav">
-              </ul>
+const App = ({ children }) =>
+  <div>
+    <nav className="navbar navbar-default">
+      <div className="container">
+        <div className="navbar-header">
+          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
+            <span className="icon-bar"/>
+            <span className="icon-bar"/>
+            <span className="icon-bar"/>
+          </button>
+          <Link to="/" className="navbar-brand">Кленинка</Link>
+        </div>
+        <div id="navbar" className="collapse navbar-collapse">
+          <ul className="nav navbar-nav">
+          </ul>
 
-              <UserMenu />
-            </div>
-          </div>
-        </nav>
-        <main className="container">
-          {this.props.children}
-        </main>
-        <footer className="container">
-          <div className="row">
-            <div className="col-xs-6">
-              2016 Кленинка<br/>
-              <a href="https://github.com/Davidyuk/witcoin/issues">Трекер ошибок</a>
-              , <a href="http://vk.com/witcoin">VK</a>
-            </div>
-            <div className="col-xs-6 text-right">
-              {/*informer.html*/}
-            </div>
-          </div>
-        </footer>
+          <UserMenu />
+        </div>
       </div>
-    );
-  }
-}
+    </nav>
+    <main className="container">
+      {children}
+    </main>
+    <footer className="container">
+      <div className="row">
+        <div className="col-xs-6">
+          2016 Кленинка<br/>
+          <a href="https://github.com/Davidyuk/witcoin/issues">Трекер ошибок</a>
+          , <a href="http://vk.com/witcoin">VK</a>
+        </div>
+        <div className="col-xs-6 text-right">
+          {/*informer.html*/}
+        </div>
+      </div>
+    </footer>
+  </div>;
 
 App.propTypes = {
   children: React.PropTypes.element,
 };
+
+export default App;
