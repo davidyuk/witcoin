ab0da32423a1b9edd79fd3130a54c0bc3595d71e
Denis <denis_davidyuk@hotmail.com>
2016-10-25 02:38:09 +1000

    UserMenu: Refactor as pure function

--- imports/ui/components/UserMenu.jsx
+++ imports/ui/components/UserMenu.jsx
@@ -2,56 +2,37 @@ import React from 'react';
 import { Link } from 'react-router';
 
 import UserName from './UserName';
+import LinkMenu from './LinkMenu';
 import { injectUser } from '../injectors';
 
-class UserMenu extends React.Component {
-  logOut(e) {
-    e.preventDefault();
-    AccountsTemplates.logout();
-    this.context.router.push('/');
-  }
-
-  renderLoggedIn() {
-    const user = this.props.user;
-    const userPagePath = '/u/' + user._id;
-    const router = this.context.router;
-
-    return <ul className="nav navbar-nav navbar-right">
+const UserMenu = ({ user }) => {
+  return user ? (
+    <ul className="nav navbar-nav navbar-right">
       <li>
         <a className="dropdown-toggle" data-toggle="dropdown" href="#">
           <UserName user={user}/>&nbsp;
           <b className="caret"/>
         </a>
         <ul className="dropdown-menu">
-          <li className={router.isActive(userPagePath) ? 'active' : ''}><Link to={userPagePath}>Моя страница</Link></li>
-          <li className={router.isActive('/feed') ? 'active' : ''}><Link to={'/feed'}>Новости</Link></li>
-          <li className={router.isActive('/notifications') ? 'active' : ''}><Link to={'/notifications'}>Уведомления</Link></li>
-          <li className={router.isActive('/im') ? 'active' : ''}><Link to={'/im'}>Сообщения</Link></li>
+          <LinkMenu to={'/u/' + user._id}>Моя страница</LinkMenu>
+          <LinkMenu to="/feed">Новости</LinkMenu>
+          <LinkMenu to="/notifications">Уведомления</LinkMenu>
+          <LinkMenu to="/im">Сообщения</LinkMenu>
           <li className="divider"/>
-          <li><a onClick={this.logOut.bind(this)} href="#">Выход</a></li>
+          <li><Link onClick={() => AccountsTemplates.logout()} to="/">Выход</Link></li>
         </ul>
       </li>
-    </ul>;
-  }
-
-  renderLoggedOut() {
-    return <ul className="nav navbar-nav navbar-right">&nbsp;
+    </ul>
+  ) : (
+    <ul className="nav navbar-nav navbar-right">&nbsp;
       <Link to="/sign-in" className="btn btn-default navbar-btn">Войти</Link>&nbsp;
       <Link to="/sign-up" className="btn btn-success navbar-btn">Регистрация</Link>
-    </ul>;
-  }
-
-  render() {
-    return this.props.user ? this.renderLoggedIn() : this.renderLoggedOut();
-  }
-}
+    </ul>
+  );
+};
 
 UserMenu.propTypes = {
   user: React.PropTypes.object,
-};
-
-UserMenu.contextTypes = {
-  router: React.PropTypes.object.isRequired,
 };
 
 export default injectUser(UserMenu);
