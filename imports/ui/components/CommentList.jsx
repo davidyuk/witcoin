e5e6400ed9af0fe9b31c8d75cdcac28694d51665
Denis <denis_davidyuk@hotmail.com>
2016-11-25 14:18:01 +1000

    CommentList: Hide previous comments

--- imports/ui/components/CommentList.jsx
+++ imports/ui/components/CommentList.jsx
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
+import { FormattedPlural } from 'react-intl';
 import { createContainer } from 'meteor/react-meteor-data';
 
 import MessageInput from './MessageInput';
@@ -7,15 +8,40 @@ import Comment from './Comment';
 import { Actions } from '../../api/actions';
 
 class CommentList extends React.Component {
+  constructor() {
+    super();
+    this.state = {showAll: false};
+    this.alwaysVisibleCount = 1;
+  }
+
   addComment(comment) {
     Meteor.call('action.comment', this.props.actionId, comment);
   }
 
-  render() {
+  renderToggleCommentsButton() {
     const comments = this.props.comments;
+    if (this.props.showAll || comments.length <= this.alwaysVisibleCount) return null;
+    return (
+      <button className="btn btn-default btn-sm btn-block" style={{marginBottom: '5px'}}
+              onClick={() => this.setState({showAll: !this.state.showAll})}>
+        {this.state.showAll ? 'Скрыть комментарии' : <span>
+          Показать {comments.length - this.alwaysVisibleCount}&nbsp;
+          <FormattedPlural value={comments.length - this.alwaysVisibleCount}
+                           one="предыдущий комментарий"
+                           few="предыдущих комментария"
+                           other="предыдущих комментариев" />
+        </span>}
+      </button>
+    );
+  }
+
+  render() {
+    const showAll = this.props.showAll || this.state.showAll;
+    const comments = this.props.comments.slice(!showAll && -this.alwaysVisibleCount);
 
     return (
       <div style={{padding: '5px 0 0 40px'}}>
+        {this.renderToggleCommentsButton()}
         <div className="list-group" style={{marginBottom: 0}}>
           {comments.map(comment =>
             <Comment comment={comment} key={comment._id} />
@@ -30,6 +56,11 @@ class CommentList extends React.Component {
 CommentList.propTypes = {
   comments: React.PropTypes.array.isRequired,
   actionId: React.PropTypes.string.isRequired,
+  showAll: React.PropTypes.bool,
+};
+
+CommentList.defaultProps = {
+  showAll: false,
 };
 
 export default createContainer(
