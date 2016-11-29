85629fd294a505e0e3ba7e7872686800eed3fcd9
Denis <denis_davidyuk@hotmail.com>
2016-11-29 19:43:22 +1000

    Add eonasdan-bootstrap-datetimepicker npm package

--- client/main.jsx
+++ client/main.jsx
@@ -1,9 +1,14 @@
 import 'bootstrap-sass';
+import moment from 'moment';
+import 'moment/locale/ru';
+import 'eonasdan-bootstrap-datetimepicker';
 
 import '../imports/startup/accounts-config';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import { renderRoutes } from '../imports/startup/client/routes.jsx';
+
+moment.locale('ru');
 
 Meteor.startup(() => {
   render(renderRoutes(), document.getElementById('app'));
