7309018e61549d4caae7ddacd5e396acecee0d26
Denis <denis_davidyuk@hotmail.com>
2016-12-04 15:15:14 +1000

    UserList: Refactor as pure function

--- imports/ui/components/UserList.jsx
+++ imports/ui/components/UserList.jsx
@@ -2,33 +2,27 @@ import React from 'react';
 
 import LinkToUser from './LinkToUser';
 
-export default class UserList extends React.Component {
-  render() {
-    const title = this.props.title;
-    const count = this.props.count;
-
-    return (
-      <div className="panel panel-default">
-        {title || count ?
-          <div className="panel-heading">
-            { title }
-            { count ? <span className="badge pull-right">{ count }</span> : null }
-          </div>
-          : null}
-        <ul className="list-group">
-          { this.props.users.map(user =>
-            <li key={user._id} className="list-group-item">
-              <LinkToUser user={user}/>
-            </li>
-          ) }
-        </ul>
+const UserList = ({title, count, users}) =>
+  <div className="panel panel-default">
+    {title || count ? (
+      <div className="panel-heading">
+        {title}
+        {count ? <span className="badge pull-right">{count}</span> : null}
       </div>
-    );
-  }
-}
+    ) : null}
+    <ul className="list-group">
+      {users.map(user =>
+        <li key={user._id} className="list-group-item">
+          <LinkToUser user={user} />
+        </li>
+      )}
+    </ul>
+  </div>;
 
 UserList.propTypes = {
   users: React.PropTypes.array.isRequired,
   title: React.PropTypes.string,
   count: React.PropTypes.number,
 };
+
+export default UserList;
