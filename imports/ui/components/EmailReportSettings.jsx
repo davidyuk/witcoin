b8f9618ca36237f8296affdb2c46f2aff0ad446f
Denis <denis_davidyuk@hotmail.com>
2016-12-06 03:48:04 +1000

    EmailReportSettings: Remove submit button

--- imports/ui/components/EmailReportSettings.jsx
+++ imports/ui/components/EmailReportSettings.jsx
@@ -4,41 +4,44 @@ import { Meteor } from 'meteor/meteor';
 import { injectUser } from '../hocs';
 
 const EmailReportSettings = ({ user }) => {
-  const updateReportsSettings = event => {
-    event.preventDefault();
-    Meteor.users.update(Meteor.userId(), {$set: {
-      'profile.reports.send.news': event.target.news.checked,
-      'profile.reports.send.notifications': event.target.notifications.checked,
-      'profile.reports.send.chats': event.target.chats.checked,
-      'profile.reports.interval': event.target.interval.value,
-      'profile.reports.newsDate': new Date((new Date).getTime() + event.target.interval.value * 1000),
-    }});
-  };
-
   const checkboxField = (placeholder, name, value) => {
     return (
       <div className="checkbox">
-        <label><input type="checkbox" name={name} defaultChecked={value} /> {placeholder}</label>
+        <label>
+          <input type="checkbox" checked={value} onChange={event =>
+            Meteor.users.update(user._id, {$set: {[name]: event.target.checked}})
+          } />
+          {placeholder}
+        </label>
       </div>
     );
   };
 
+  const send = user.profile.reports.send;
+  const enabled = Object.values(send).find(a => a);
+
   return <div>
     <h2>Уведомления</h2>
     Все уведомления приходят в одном письме на основной адрес электронной почты.
-    <form className="form-horizontal" onSubmit={updateReportsSettings}>
+    <div className="form-horizontal">
       <div className="form-group">
         <label className="col-sm-3 control-label">Уведомлять о</label>
         <div className="col-sm-9">
-          {checkboxField('действиях тех, на кого я подписан', 'news', user.profile.reports.send.news)}
-          {checkboxField('ответах на мои действия', 'notifications', user.profile.reports.send.notifications)}
-          {checkboxField('непрочитанных мною сообщениях', 'chats', user.profile.reports.send.chats)}
+          {checkboxField('действиях тех, на кого я подписан', 'profile.reports.send.news', send.news)}
+          {checkboxField('ответах на мои действия', 'profile.reports.send.notifications', send.notifications)}
+          {checkboxField('непрочитанных мною сообщениях', 'profile.reports.send.chats', send.chats)}
         </div>
       </div>
       <div className="form-group">
         <label className="col-sm-3 control-label">Отправлять письмо</label>
         <div className="col-sm-9">
-          <select defaultValue={user.profile.reports.interval} className="form-control" name="interval">
+          <select className="form-control" disabled={!enabled} value={user.profile.reports.interval}
+                  onChange={event =>
+                    Meteor.users.update(Meteor.userId(), {$set: {
+                      'profile.reports.interval': event.target.value,
+                      'profile.reports.newsDate': new Date(Date.now() + event.target.value * 1000),
+                    }})
+                  }>
             <option value={0}>как можно чаще</option>
             <option value={60 * 20}>один раз в 20 минут</option>
             <option value={60 * 60}>один раз в час</option>
@@ -46,12 +49,7 @@ const EmailReportSettings = ({ user }) => {
           </select>
         </div>
       </div>
-      <div className="form-group">
-        <div className="col-sm-offset-3 col-sm-9">
-          <button className="btn btn-default" type="submit">Сохранить</button>
-        </div>
-      </div>
-    </form>
+    </div>
   </div>;
 };
 
