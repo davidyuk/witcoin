051c4e74076c48c497dfdd4c63136a46c7182a13
Denis <denis_davidyuk@hotmail.com>
2016-12-05 13:59:47 +1000

    homepage: Add list of online users, counters of online users and connections

--- imports/ui/pages/HomePage.jsx
+++ imports/ui/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import Helmet from 'react-helmet';
 
 import UserList from '../components/UserList';
 
-const HomePage = ({counts, usersLast}) =>
+const HomePage = ({counts, usersLast, usersOnline}) =>
   <div className="row">
     <Helmet title="Привет!" />
     <div className="col-sm-8">
@@ -23,6 +23,14 @@ const HomePage = ({counts, usersLast}) =>
             </td>
           </tr>
           <tr>
+            <td>В сети</td>
+            <td>
+              {counts.usersOnline}&nbsp;
+              <FormattedPlural value={counts.usersOnline}
+                               one="пользователь" few="пользователя" other="пользователей" />
+            </td>
+          </tr>
+          <tr>
             <td>Отправлено</td>
             <td>
               {counts.messages}&nbsp;
@@ -30,9 +38,20 @@ const HomePage = ({counts, usersLast}) =>
                                one="сообщение" few="сообщения" other="сообщений" />
             </td>
           </tr>
+          <tr>
+            <td>
+              К серверу <FormattedPlural value={counts.connections} one="подключен" other="подключено" />
+            </td>
+            <td>
+              {counts.connections}&nbsp;
+              <FormattedPlural value={counts.connections}
+                               one="клиент" few="клиента" other="клиентов" />
+            </td>
+          </tr>
           </tbody>
         </table>
       </div>
+      <UserList users={usersOnline} title="В сети" />
       <UserList users={usersLast} title="Недавно зарегистрировавшиеся" />
     </div>
   </div>;
@@ -40,17 +59,23 @@ const HomePage = ({counts, usersLast}) =>
 HomePage.propTypes = {
   counts: React.PropTypes.shape({
     users: React.PropTypes.number,
+    usersOnline: React.PropTypes.number,
     messages: React.PropTypes.number,
+    connections: React.PropTypes.number,
   }),
   usersLast: React.PropTypes.array,
+  usersOnline: React.PropTypes.array,
 };
 
 HomePage.defaultProps = {
   counts: {
     users: 0,
+    usersOnline: 0,
     messages: 0,
+    connections: 0,
   },
   usersLast: [],
+  usersOnline: [],
 };
 
 export default HomePage;
