546c4b7860f9952704c7bc6f826c31a7b076eebf
Denis <denis_davidyuk@hotmail.com>
2016-12-05 13:59:47 +1000

    homepage: Add list of online users, counters of online users and connections

--- imports/api/homepage.js
+++ imports/api/homepage.js
@@ -1,11 +1,14 @@
 import { Meteor } from 'meteor/meteor';
+import { UserStatus } from 'meteor/mizzao:user-status';
 
 import { Messages } from './chats';
 
 if (Meteor.isServer) {
   Meteor.publish('homePage.counters', function() {
     Counts.publish(this, 'users', Meteor.users.find());
+    Counts.publish(this, 'users.online', Meteor.users.find({'status.online': true}));
     Counts.publish(this, 'messages', Messages.find());
+    Counts.publish(this, 'connections', UserStatus.connections.find());
   });
 
   Meteor.publish('homePage.users.last', () =>
@@ -13,6 +16,13 @@ if (Meteor.isServer) {
       sort: {createdAt: -1},
       limit: 5,
       fields: {...Meteor.users.publicFields, createdAt: 1},
+    })
+  );
+
+  Meteor.publish('homePage.users.online', () =>
+    Meteor.users.find({'status.online': true}, {
+      limit: 5,
+      fields: Meteor.users.publicFields,
     })
   );
 }
--- imports/ui/containers/HomePageContainer.jsx
+++ imports/ui/containers/HomePageContainer.jsx
@@ -4,11 +4,15 @@ import HomePage from '../pages/HomePage';
 export default HomePageContainer = createContainer(() => {
   Meteor.subscribe('homePage.counters');
   const usersLastReady = Meteor.subscribe('homePage.users.last').ready();
+  const usersOnlineReady = Meteor.subscribe('homePage.users.online').ready();
   return {
     usersLast: usersLastReady ? Meteor.users.find({}, {sort: {createdAt: -1}, limit: 5}).fetch() : [],
+    usersOnline: usersOnlineReady ? Meteor.users.find({'status.online': true}, {limit: 5}).fetch() : [],
     counts: {
       users: Counts.get('users'),
+      usersOnline: Counts.get('users.online'),
       messages: Counts.get('messages'),
+      connections: Counts.get('connections'),
     },
   };
 }, HomePage);
